501f59a74777d60abac9e8a29672fb8404a6658d
Daphne <dolllikewawa@gmail.com>
2018-05-13 14:34:14 -0700

    calling the second loadFeed() in thebeforeEach as well and then compare both variables.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,5 +1,4 @@
-/* feedreader.js
- *
+/* feedreader.js *
  * This is the spec file that Jasmine will read and contains
  * all of the tests that will be run against your application.
  */
@@ -17,10 +16,6 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
         it('has a URL and that URL is not empty', function() {
             allFeeds.forEach((feed) => {
                 expect(feed.url).toBeDefined();
@@ -28,10 +23,6 @@ $(function() {
             });
         });
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
         it('has a name and that name is not empty', function() {
             allFeeds.forEach((feed) => {
                 expect(feed.name).toBeDefined();
@@ -40,17 +31,12 @@ $(function() {
         });
     });
     
-    /* TODO: Write a new test suite named "The menu" */
-    describe('The menu', function() {    
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. 
-         */
+        describe('The menu', function() {    
+
         it('is hidden by default', function() {
             expect($('body').hasClass('menu-hidden')).toBe(true);
         });
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked.
-          */
+
         it('shows up when click on the menu icon', function() {
             $('.menu-icon-link').click();
             expect($('body').hasClass('menu-hidden')).toBe(false);
@@ -62,12 +48,8 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         */
+
         beforeEach(function(done) {
             setTimeout(function() {
                 loadFeed(0, done);
@@ -80,24 +62,22 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function() {
-        let feedContent;    
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         */
+        let oldFeed, newFeed;    
+
         beforeEach(function(done) {
             loadFeed(0, function() {
-                feedContent = $('.feed').html();
+                oldFeed = $('.feed').html();
+            loadFeed(1, function() {
+                newFeed = $('.feed').html();
                 done();
+                });                
             });           
         });
 
         it ('changes content', function(done) {
-            loadFeed(1, function() {
-                expect($('.feed').html()).not.toEqual(feedContent);
-                done();
-            });
+            expect(oldFeed).not.toEqual(newFeed);
+            done();            
         });
     });
 
